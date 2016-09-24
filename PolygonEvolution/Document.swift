1ee056979ee2d5da0aaac3e5327a2d30a2cafa18
Stefan Hintz <github@corbusierhaus.de>
2016-09-24 14:14:39 +0200

    Read and write

--- PolygonEvolution/Document.swift
+++ PolygonEvolution/Document.swift
@@ -6,37 +6,42 @@
 
 import Cocoa
 
-class Document: NSDocument {
-
-	override init() {
-	    super.init()
-		// Add your subclass-specific initialization here.
-	}
-
-	override class func autosavesInPlace() -> Bool {
+class Document: NSDocument
+{
+	override class func autosavesInPlace() -> Bool
+	{
 		return true
 	}
 
-	override func makeWindowControllers() {
+	override func makeWindowControllers()
+	{
 		// Returns the Storyboard that contains your Document window.
 		let storyboard = NSStoryboard(name: "Main", bundle: nil)
 		let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
 		self.addWindowController(windowController)
 	}
 
-	override func data(ofType typeName: String) throws -> Data {
-		// Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-		// You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+	override func data(ofType typeName: String) throws -> Data
+	{
+		let string = ""
+
+		if let value = string.data(using: String.Encoding.utf8)
+		{
+			return value
+		}
+
+		throw NSError(domain: "PolygonEvolution", code: 1, userInfo: nil)
 	}
 
-	override func read(from data: Data, ofType typeName: String) throws {
-		// Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-		// You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-		// If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+	override func read(from data: Data, ofType typeName: String) throws
+	{
+		if let string = String(data: data, encoding: String.Encoding.utf8)
+		{
+
+			return
+		}
+
+		throw NSError(domain: "PolygonEvolution", code: 2, userInfo: nil)
 	}
-
-
 }
 
