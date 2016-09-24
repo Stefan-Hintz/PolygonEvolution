bfdb0aafd248e7b9f02fedaf12ae15607b62b7ec
Morris Winkler <m.winkler@laydrop.com>
2016-09-24 14:55:29 +0200

    JSONJoy

--- PolygonEvolution/Models/world.swift
+++ PolygonEvolution/Models/world.swift
@@ -7,33 +7,77 @@
 //
 
 import Foundation
-
+import JSONJoy
 
 public typealias Scalar = Float
 
-public struct Vector2 {
+public struct Vector2: JSONJoy {
     public var x: Scalar
     public var y: Scalar
+    
+    public init(_ decoder: JSONDecoder) throws {
+        x = try decoder["x"].getFloat()
+        y = try decoder["x"].getFloat()
+    }
 }
 
-public typealias Edge = [Vector2]
+public struct Edge: JSONJoy {
+    public var vertices: [Vector2]
+    
+    public init(_ decoder: JSONDecoder) throws {
+        guard let _vertices = decoder["vertices"].array else {
+            throw  JSONError.wrongType
+        }
+        
+        var collect = [Vector2]()
+        for vertDecoder in _vertices {
+            try collect.append(Vector2(vertDecoder))
+        }
+        
+        vertices = collect
+    }
+}
 
+public struct ShapeType: JSONJoy {
+    public var name: String
+    public var id: String
+    
+    public init(_ decoder: JSONDecoder) throws {
+        name = try decoder["name"].getString()
+        id = try decoder["uuid"].getString()
+    }
+}
 
-public struct Shape {
+public struct Shape: JSONJoy{
     public var edges: [Edge]
     public var center: Vector2
-    public var id: UUID
+    public var type: ShapeType
+    
+    public init(_ decoder: JSONDecoder) throws {
+        
+        guard let _edges = decoder["edges"].array else {throw JSONError.wrongType}
+        var collect = [Edge]()
+        for edgeDecoder in _edges {
+            try collect.append(Edge(edgeDecoder))
+        }
+        edges = collect
+        center = try Vector2(decoder["center"])
+        type = try ShapeType(decoder["type"])
+    }
 }
 
 
 
 class World
 {
-    //var Shapes: Shape
-    var Worldfile: String
+    var Shapes: Shape
+    //var Worldfile: String
     
-    
-    init(file: String) {
-        self.Worldfile = file
+    init?(jsonString: String) {
+        do {
+            Shapes = try Shape(JSONDecoder(jsonString))
+        } catch {
+            return nil
+        }
     }
 }
