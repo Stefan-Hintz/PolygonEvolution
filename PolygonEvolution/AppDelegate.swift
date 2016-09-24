154a0ae7e43c658d774deb53cf9190a53517f3e0
Stefan Hintz <github@corbusierhaus.de>
2016-09-24 10:47:49 +0200

    Initial Commit

---
+++ PolygonEvolution/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PolygonEvolution
+//
+//  Created by Stefan Hintz on 24.09.16.
+//  Copyright © 2016 Stefan Hintz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ PolygonEvolution/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PolygonEvolution
+//
+//  Created by Stefan Hintz on 24.09.16.
+//  Copyright © 2016 Stefan Hintz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
