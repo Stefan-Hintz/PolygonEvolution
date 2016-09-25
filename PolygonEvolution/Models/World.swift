f75bb2b1ef29fc4c04cc46e1426ffcb83fb2dfbe
Morris Winkler <m.winkler@laydrop.com>
2016-09-25 12:00:56 +0200

    wire

--- PolygonEvolution/Models/World.swift
+++ PolygonEvolution/Models/World.swift
@@ -10,7 +10,7 @@ import Foundation
 
 typealias Scalar = Double
 
-class Vector2
+class Vector2: Hashable
 {
     var x = 0.0
     var y = 0.0
@@ -19,6 +19,24 @@ class Vector2
     {
         self.x = x
         self.y = y
+    }
+    
+    func add(v: Vector2)
+    {
+        x = x + v.x
+        y = y + v.y
+    }
+    
+    func sub(v: Vector2)
+    {
+        x = x - v.x
+        y = y - v.y
+    }
+    
+    func scale(s: Double)
+    {
+        x = x*s
+        y = y*s
     }
     
     func fromJSON(object: [String: Any])
@@ -43,15 +61,48 @@ class Vector2
         
         return object
     }
+    
+    // make class Hashable
+    var hashValue: Int
+        {
+        get {
+            return (self.x + self.y).hashValue
+        }
+    }
+    
+    // make class Equatable
+    public static func ==(lhs: Vector2, rhs: Vector2) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
+    }
 }
 
-class Edge
+class Edge: Hashable
 {
+    var shape = Shape()
     var start = Vector2()
     var end = Vector2()
 
-    func setEdge(start: Vector2, end: Vector2)
+    var worldStart = Vector2()
+    var worldEnd = Vector2()
+    
+    
+    func setWorldCoords(center: Vector2)
     {
+    
+            worldStart.scale(s: SIZE)
+            worldStart.add(v: center)
+            worldStart.sub(v: shape.center)
+    
+            worldEnd.scale(s: SIZE)
+            worldEnd.add(v: center)
+            worldEnd.sub(v: shape.center)
+            return self.worldEnd
+    }
+    
+    func setEdge(start: Vector2, end: Vector2, shape: Shape)
+    {
+        
+        self.shape = shape
         self.start = start
         self.end = end
     }
@@ -70,6 +121,20 @@ class Edge
         object["end"] = end.toJSON()
         
         return object
+    }
+    
+    // make class Hashable
+    var hashValue: Int
+        {
+        get {
+            return ((self.worldStart.x + self.worldEnd.x)*(self.worldStart.y + self.worldEnd.y)).hashValue
+        }
+    }
+    
+    // make class Equatable
+    public static func ==(lhs: Edge, rhs: Edge) -> Bool
+    {
+        return (lhs.start.x == rhs.start.x && lhs.end.x == rhs.end.x) && (lhs.start.y == rhs.start.y && lhs.end.y == rhs.end.y)
     }
 }
 
@@ -101,6 +166,23 @@ class Shape
     var vertices = [Vector2]()
     var center = Vector2()
     var type = ShapeType()
+    
+    var worldCenter: Vector2
+    {
+        set(center)
+        {
+            self.worldCenter = center
+            for edge in edges
+            {
+                edge.setWorldCoords(center: center)
+            }
+            
+        }
+        get
+        {
+            return self.worldCenter
+        }
+    }
 
     func fromJSON(object: [String: Any])
     {
@@ -182,12 +264,12 @@ class Shape
         
         for i in 0..<count-1  {
             let e = Edge()
-            e.setEdge(start: vertices[i], end: vertices[i+1])
+            e.setEdge(start: vertices[i], end: vertices[i+1], shape: self)
             edges.append(e)
         }
         
         let e = Edge()
-        e.setEdge(start: vertices[count-1], end: vertices[0])
+        e.setEdge(start: vertices[count-1], end: vertices[0], shape: self)
         edges.append(e)
     }
     
@@ -211,6 +293,63 @@ class World
 {
     var name = ""
     var shapes = [Shape]()
+    
+    var generation = 1
+    var generationShapes: [Int: [Shape]] = [0: [Shape]()]
+    
+    var edgeNeighbours: [Edge: [Shape]] = [Edge(): [Shape]()]
+    var vertexNeighbours: [Vector2: [Shape]] = [Vector2(): [Shape]()]
+    
+    var scale: Int = 1
+    
+    
+    func evolve() -> ([Vector2], [Shape])
+    {
+
+        generation = generation + 1
+        var eVecs = [Vector2]()
+        var eShapes = [Shape]()
+
+        for shape in shapes
+        {
+            for edge in shape.edges
+            {
+                if (edgeNeighbours[edge]?.count)! < 2
+                {
+                    eVecs.append(edge.worldStart)
+                    eVecs.append(edge.worldEnd)
+                }
+            }
+        }
+        
+        return (eVecs, eShapes)
+        
+    }
+    
+    func addShape(shape: Shape, center: Vector2)
+    {
+        shape.worldCenter = center
+        shapes.append(shape)
+        addEdgeNeighbours(s: shape)
+        addVerticesNeighbours(s: shape)
+        generationShapes[generation]?.append(shape)
+    }
+    
+    func addEdgeNeighbours(s: Shape)
+    {
+        for e in s.edges
+        {
+            edgeNeighbours[e]?.append(s)
+        }
+    }
+    
+    func addVerticesNeighbours(s: Shape)
+    {
+        for v in s.vertices
+        {
+                vertexNeighbours[v]?.append(s)
+        }
+    }
     
     func fromJSON(object: [String: Any])
 	{
